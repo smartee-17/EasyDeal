98885f0857af08b995decee45aa2b844ca5143c7
somaia02 <somaia0246@gmail.com>
2026-04-15 04:19:05 +0200

    Handle form submission

--- frontend/Auth/auth.js
+++ frontend/Auth/auth.js
@@ -1,4 +1,31 @@
+import { loginUser } from "./authService.js"; 
+
+/* Handle dark mode based on system preference */
 const systemDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
 document.documentElement.setAttribute('data-theme', systemDark ? 'dark' : 'light');
+
+/* Login form submission */
+const loginForm = document.querySelector('.login__form');
+loginForm.addEventListener('submit', async (e) => {
+  e.preventDefault();
+
+  const formData = new FormData(loginForm);
+  const credentials = Object.fromEntries(formData);
+
+
+  try {
+    const data = await loginUser(credentials);
+    
+    // Success: Store the JWT and redirect
+    localStorage.setItem('token', data.token);
+
+    // TODO: Implement proper redirection logic based on user role and redirect to the appropriate dashboard
+    window.location.href = '../index.html';
+  } catch (err) {
+    // UI Error Handling
+    document.querySelector('.login__error-msg').textContent = err.message;
+  }
+});
+
 
 
