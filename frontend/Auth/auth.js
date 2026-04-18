fc38a26af7a39852b7be794187229634e030d670
somaia02 <somaia0246@gmail.com>
2026-04-19 01:04:06 +0200

    Add password visibility toggle

--- frontend/Auth/auth.js
+++ frontend/Auth/auth.js
@@ -1,8 +1,25 @@
 import { loginUser } from "./authService.js"; 
+import { eyeIcon, eyeSlashIcon } from "./icons.js";
 
 /* Handle dark mode based on system preference */
 const systemDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
 document.documentElement.setAttribute('data-theme', systemDark ? 'dark' : 'light');
+
+/* Password visibility toggle */
+const togglePasswordBtn = document.querySelector('.login__form__toggle-password');
+const passwordInput = document.querySelector('.login__form__password-input');
+togglePasswordBtn.addEventListener('click', () => {
+  const isPasswordVisible = passwordInput.type === 'text';
+  if (isPasswordVisible) {
+    passwordInput.type = 'password';
+    togglePasswordBtn.setAttribute('aria-label', 'Show password as plain text. Warning: this will display your password on the screen.');
+    togglePasswordBtn.innerHTML = eyeIcon;
+  } else {
+    passwordInput.type = 'text';
+    togglePasswordBtn.setAttribute('aria-label', 'Hide password');
+    togglePasswordBtn.innerHTML = eyeSlashIcon;
+  }
+});
 
 /* Login form submission */
 const loginForm = document.querySelector('.login__form');
