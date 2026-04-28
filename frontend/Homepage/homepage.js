cf86eddf2daa2cfe5762650846cafe733552a55c
Nathan-coder691 <takelebayelign@gmail.com>
2026-04-28 18:28:09 +0300

    Refactor: align products.json with backend schema and update JS mapping

--- frontend/Homepage/homepage.js
+++ frontend/Homepage/homepage.js
@@ -1,18 +1,20 @@
-const products = document.querySelector('.products');
-const categories = document.querySelector('.category-bar__list');
+const products = document.querySelector(".products");
+const categories = document.querySelector(".category-bar__list");
 
 async function fetchData() {
-  let res = await fetch('products.json');
-  let data = await res.json();
+  let res = await fetch("products.json");
+  let result = await res.json();
+  let data = result.data;
+  products.innerHTML = "";
   data.map((a) => {
     products.innerHTML += `
-            <div class="product-card" id = ${a.id}>
+            <div class="product-card" id = ${a._id}>
           <img
             class="product-card__image"
-            src= "${a.image}"
-            alt="Mouse"
+            src= "${a.images[0]}"
+            alt="${a.altText}"
           />
-          <h3 class="product-card__title">${a.name}</h3>
+          <h3 class="product-card__title">${a.title}</h3>
 
           <p class="product-card__price product-card__price--sale">${a.price}</p>
 
@@ -23,38 +25,38 @@ async function fetchData() {
     `;
   });
   let category = [...new Set(data.map((items) => items.category))];
-  category.unshift('All Products');
-  console.log(category);
+  category.unshift("All Products");
+
   const categoryHTML = category
     .map((cat) => {
       return ` <li class="category-bar__item">${cat}</li>`;
     })
-    .join('');
+    .join("");
   categories.innerHTML += categoryHTML;
 
-  let categoryList = document.querySelectorAll('.category-bar__item');
+  let categoryList = document.querySelectorAll(".category-bar__item");
   categoryList.forEach((a) => {
-    if (a.textContent === 'All Products') {
-      a.classList.add('active');
+    if (a.textContent === "All Products") {
+      a.classList.add("active");
     }
-    a.addEventListener('click', () => {
+    a.addEventListener("click", () => {
       categoryList.forEach((btn) => {
-        btn.classList.remove('active');
+        btn.classList.remove("active");
       });
 
-      a.classList.add('active');
+      a.classList.add("active");
 
-      products.innerHTML = '';
-      if (a.textContent === 'All Products') {
+      products.innerHTML = "";
+      if (a.textContent === "All Products") {
         data.map((a) => {
           products.innerHTML += `
-            <div class="product-card" id = ${a.id}>
+            <div class="product-card" id = ${a._id}>
           <img
             class="product-card__image"
-            src= "${a.image}"
-            alt="Mouse"
+            src= "${a.images[0]}"
+            alt="${a.altText}"
           />
-          <h3 class="product-card__title">${a.name}</h3>
+          <h3 class="product-card__title">${a.title}</h3>
 
           <p class="product-card__price product-card__price--sale">${a.price}</p>
 
@@ -68,13 +70,13 @@ async function fetchData() {
       data.map((d) => {
         if (d.category === a.textContent) {
           products.innerHTML += `
-            <div class="product-card" id = ${d.id}>
+            <div class="product-card" id = ${d._id}>
           <img
             class="product-card__image"
-            src= "${d.image}"
-            alt="Mouse"
+            src= "${d.images[0]}"
+            alt="${d.altText}"
           />
-          <h3 class="product-card__title">${d.name}</h3>
+          <h3 class="product-card__title">${d.title}</h3>
 
           <p class="product-card__price product-card__price--sale">${d.price}</p>
 
