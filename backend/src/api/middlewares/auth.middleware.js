72af1f9b68e51635e5af4b6b952a0cf9ec2ea44a
Rajiv <kumarrajiv0920@gmail.com>
2026-04-29 19:58:14 +0530

    fix(auth): add async to protect middleware function

--- backend/src/api/middlewares/auth.middleware.js
+++ backend/src/api/middlewares/auth.middleware.js
@@ -1,28 +1,31 @@
 import jwt from 'jsonwebtoken';
-import User from '../models/user.model';
+import User from '../models/user.model.js';
 
-const protect = (req, res, next) => {
+const protect = async (req, res, next) => {
   try {
     const authHeaders = req.headers.authorization;
 
-    let token = req.cookies?.token || (authHeaders && authHeaders.startsWith('Bearer ') ? authHeaders.split(' ')[1] : null);
+    let token =
+      req.cookies?.token ||
+      (authHeaders && authHeaders.startsWith('Bearer ')
+        ? authHeaders.split(' ')[1]
+        : null);
 
-    if(!token) return res.status(401).json({ message: "Not authorized, no token" });
+    if (!token)
+      return res.status(401).json({ message: 'Not authorized, no token' });
 
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
     req.user = await User.findById(decoded.id).select('name email role');
 
     if (!req.user) {
-      return res.status(401).json({message: 'User not found'})
+      return res.status(401).json({ message: 'User not found' });
     }
 
     next();
-
-  } catch(error) {
-     return res.status(401).json({ message: "Not authorized, token failed" });
+  } catch (error) {
+    return res.status(401).json({ message: 'Not authorized, token failed' });
   }
-}
-
+};
 
 export default protect;
