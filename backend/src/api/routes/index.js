d4bfee66972847e470787ceaffe61024b82a98d3
smartee-17 <202785483+smartee-17@users.noreply.github.com>
2026-06-01 16:25:49 +0100

    update: updated the index.js in routes to accomodate user.routes as profileRoutes

--- backend/src/api/routes/index.js
+++ backend/src/api/routes/index.js
@@ -2,11 +2,14 @@ import express from 'express';
 
 import productRoutes from './product.route.js';
 import authRoutes from './auth.route.js';
+import profileRoutes from './user.route.js'
 
 const router = express.Router();
 
 router.use('/auth', authRoutes);
 
 router.use('/products', productRoutes);
+
+router.use('/profile', profileRoutes);
 
 export default router;
