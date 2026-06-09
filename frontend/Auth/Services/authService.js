d7fdcddec062a19954b5d7f09a3fc9ca08ed1648
Rajiv <kumarrajiv0920@gmail.com>
2026-06-09 22:43:07 +0530

    fix(login): Add API to login route and fix the payload

--- frontend/Auth/Services/authService.js
+++ frontend/Auth/Services/authService.js
@@ -1,8 +1,9 @@
+const API_URL = 'https://easydeal.onrender.com/api/auth';
 export async function registerUser(credentials) {
-  const response = await fetch('https://easydeal.onrender.com/api/auth/register', {
+  const response = await fetch(`${API_URL}/register`, {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify(credentials)
+    body: JSON.stringify(credentials),
   });
 
   await checkResponseStatus(response);
@@ -12,15 +13,19 @@ export async function registerUser(credentials) {
 
 export async function loginUser(credentials) {
   // TODO: Replace the URL with the actual backend endpoint for authentication '/api/auth/login'
-  const response = await fetch('login.json', {
+  let payload = {
+    emailOrUsername: credentials.email,
+    password: credentials.password,
+  };
+  const response = await fetch(`${API_URL}/login`, {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify(credentials)
+    body: JSON.stringify(payload),
   });
 
   await checkResponseStatus(response);
   const data = await response.json();
-  return data;
+  return data.data;
 }
 
 async function checkResponseStatus(response) {
@@ -28,7 +33,7 @@ async function checkResponseStatus(response) {
     return;
   }
 
-  let message = "";
+  let message = '';
   try {
     const errorBody = await response.json();
     if (errorBody?.message) {
@@ -38,16 +43,15 @@ async function checkResponseStatus(response) {
     // ignore parse errors and use the fallback message
   }
 
-  if (message !== "") {
+  if (message !== '') {
     throw new Error(message);
   }
   if (response.status === 401) {
     throw new Error('Invalid email or password.');
-  } 
+  }
   if (response.status === 500) {
     throw new Error('Server is down. Please try again later.');
   }
   // Fallback for other errors (404, 403, etc.)
   throw new Error('Something went wrong. Error code: ' + response.status);
 }
-
