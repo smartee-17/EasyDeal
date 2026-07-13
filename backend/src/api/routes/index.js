216f6e4c93abe7a182d03952b3adbfd625a1852d
Rajiv Kumar <133740418+Rajiv-0920@users.noreply.github.com>
2026-07-13 13:13:27 +0530

    feat(admin): add product moderation routes (GET/DELETE) (#54)

--- backend/src/api/routes/index.js
+++ backend/src/api/routes/index.js
@@ -6,6 +6,7 @@ import userRoutes from './user.route.js'
 import categoryRoutes from './category.route.js';
 import savedRoutes from './saved.route.js';
 import tagRoutes from './tag.route.js';
+import adminRoutes from './admin.route.js';
 
 const router = express.Router();
 
@@ -20,5 +21,7 @@ router.use('/categories', categoryRoutes);
 router.use('/tags', tagRoutes);
 
 router.use('/saved', savedRoutes);
+
+router.use('/admin', adminRoutes);
 
 export default router;
